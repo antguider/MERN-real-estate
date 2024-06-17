5e042f681dde0be7ad8c9e16e5cf200f157c0c5d
Muthukumar J <antguider@gmail.com>
2024-06-17 12:15:37 +0530

    Route added

--- api/app.js
+++ api/app.js
@@ -4,6 +4,10 @@ const app = new express();
 
 console.log("te4st")
 
+app.use('/api/test', (req, res) => {
+    res.send("API called");
+});
+
 app.listen(8800, ()=> {
     console.log("Server listening")
 });
