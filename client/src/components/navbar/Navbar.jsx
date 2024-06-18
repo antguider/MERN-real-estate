c9ae7f32b310cd37f2265596b4981e5ed0a7d25e
Muthukumar J <antguider@gmail.com>
2024-06-18 22:03:24 +0530

    css change

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,8 +4,23 @@ const Navbar = () => {
     console.log("Navbar");
     return (
         <div className="navbar">
-            <div className="left">Left</div>
-            <div className="right">Right</div>
+            <div className="left">
+                <div className="logo">
+                    <img src="./logo.png" alt="MERN Estate" />
+                    <h4>MERN Estate</h4>
+                </div>
+                <span>Home</span>
+                <span>About</span>
+                <span>Contact</span>
+                <span>Agents</span>
+            </div>
+            <div className="right">
+                <div className="usr">
+                    <img src="./profile_user.png" alt="Muthukumar J" />
+                    <span className="name">Muthukumar J</span>
+                </div>
+                <button type="button">Profile</button>
+            </div>
         </div>
     )
 };
